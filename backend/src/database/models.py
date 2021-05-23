29bdcc73c33708f498d476a81df92b3a92846331
Gregory Thomas <gdenvert13@gmail.com>
2021-05-23 16:43:54 -0400

    Adding the auth and models

---
+++ backend/src/database/models.py
@@ -0,0 +1,71 @@
+import os
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASS = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'restaurants')
+database_path = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    """
+    setup_db(app)
+        binds a flask application and a SQLAlchemy service
+    """
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    hours = Column(String)
+    restauranteer = Column(String)
+
+    def __init__(self, name, hours):
+        self.name = name
+        self.hours = hours
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'name': self.name,
+            'hours': self.hours,
+            'restauranteer': self.restauranteer,
+        }
+
+
+class Restauranteer(db.Model):
+    __tablename__ = 'restauranteers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __init__(self, name):
+        self.name = name
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
